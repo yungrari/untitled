96a60b45df58cbe0a1dbef0ec7d8d1e35baedfbe
Young Emo <youngemo@Youngs-MacBook-Pro.local>
2024-08-03 00:13:23 +0200

    Update quotes

--- app/dashboard/[address]/page.tsx
+++ app/dashboard/[address]/page.tsx
@@ -7,23 +7,21 @@ import {
   CardTitle,
 } from "@/components/ui/card"
 
-export default function Page({ params }: { params: { address: string } }) {
+export default function Address({ params }: { params: { address: string } }) {
   return (
-    <>
-      <main className="flex flex-col h-screen justify-center items-center">
-        <Card>
-          <CardHeader>
-            <CardTitle>Card Title</CardTitle>
-            <CardDescription>Card Description</CardDescription>
-          </CardHeader>
-          <CardContent>
-            <code>{params.address}</code> 
-          </CardContent>
-          <CardFooter>
-            <p>Card Footer</p>
-          </CardFooter>
-        </Card>
-      </main>
-    </>
+    <main className="flex flex-col h-screen justify-center items-center">
+      <Card>
+        <CardHeader>
+          <CardTitle>Card Title</CardTitle>
+          <CardDescription>Card Description</CardDescription>
+        </CardHeader>
+        <CardContent>
+          <code>{params.address}</code> 
+        </CardContent>
+        <CardFooter>
+          <p>Card Footer</p>
+        </CardFooter>
+      </Card>
+    </main>
   )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -5,17 +5,17 @@ import { Button } from "@/components/ui/button";
 import { Globe } from "@/components/ui/globe";
 
 export default function Home() {
-  const latitude = headers().get("x-latitude") || ''
-  const longitude = headers().get("x-longitude") || ''
+  const latitude = headers().get("x-latitude") || ""
+  const longitude = headers().get("x-longitude") || ""
 
   return (
     <main className="flex flex-col h-screen justify-center items-center">
       <h1 className="font-bold leading-tight uppercase">Welcome to the %PROJECT_TITLE%!</h1>
 
-      <p>Your current location is{' '}
+      <p>Your current location is{" "}
         {latitude && longitude 
           ? <code>{JSON.stringify({ latitude, longitude })}</code> 
-          : 'unknown'}
+          : "unknown"}
       </p>
 
       <Globe latitude={+latitude} longitude={+longitude} />
