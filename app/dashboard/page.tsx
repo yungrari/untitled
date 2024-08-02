0c2100161b6e78e1ea59d4ac8c4920119059045d
Young Emo <youngemo@Youngs-MacBook-Pro.local>
2024-08-03 00:33:43 +0200

    Update dashboard

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,7 +1,13 @@
+import { Button } from "@/components/ui/button"
+import { Input } from "@/components/ui/input"
+
 export default function Dashboard() {
   return (
-    <main className="flex flex-col h-screen justify-center items-center">
-      Input
+    <main className="flex flex-col h-screen justify-center items-center p-4">
+      <div className="flex w-full max-w-md items-center space-x-2">
+        <Input type="text" placeholder="Address" />
+        <Button type="submit">Search</Button>
+      </div>
     </main>
   );
 }
