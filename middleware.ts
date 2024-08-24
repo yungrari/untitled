f2095b35812614b0a40ec21454ba2fd87b3efa3c
Young Emo <youngemo@Youngs-MacBook-Pro.local>
2024-08-25 01:33:31 +0200

    Update middleware

--- middleware.ts
+++ middleware.ts
@@ -2,36 +2,28 @@ import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
  
 export function middleware(request: NextRequest) {
-  // if (request.geo?.latitude && request.geo?.longitude) {
-  //   const response = NextResponse.next()
-
-  //   response.headers.set("x-latitude", request.geo.latitude)
-  //   response.headers.set("x-longitude", request.geo.longitude)
-
-  //   return response
-  // }
-
   const authCookie = request.cookies.get('auth');
-  
-  // Allow access to the home page for all users
-  if (request.nextUrl.pathname === '/') {
-    return NextResponse.next();
+  const response = NextResponse.next();
+
+  if (request.geo?.latitude && request.geo?.longitude) {
+    response.headers.set("x-latitude", request.geo.latitude);
+    response.headers.set("x-longitude", request.geo.longitude);
   }
-  
-  // For all other routes, check if the auth cookie is present
+
+  if (request.nextUrl.pathname === '/') {
+    return response;
+  }
+
   if (!authCookie) {
-    // If not, redirect to the home page with the "auth" query parameter
     const url = request.nextUrl.clone();
     url.pathname = '/';
     url.searchParams.set('auth', 'required');
     return NextResponse.redirect(url);
   }
-  
-  // If the auth cookie is present, allow the request to proceed
-  return NextResponse.next();
+
+  return response;
 }
  
-// Specify which routes this middleware should run on
 export const config = {
   matcher: [
     /*
